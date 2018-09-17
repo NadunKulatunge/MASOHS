95018d5c3a735ef6a21b2290c0c52cc6d408ead8
Mudalige Charana Nadun Udayanga Kulatunge <nadunku.16@cse.mrt.ac.lk>
2018-09-17 22:53:01 +0530

    Settings Screen

--- app/screens/Settings/SettingsScreen.js
+++ app/screens/Settings/SettingsScreen.js
@@ -1,25 +1,68 @@
 import React, { Component } from "react";
 import { 
   View,
-  Text,
   StyleSheet
 } from "react-native";
 
+import { Container, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch, Button } from 'native-base';
+import RightHeaderButtons from '../../components/RightHeaderButtons.js';
+
 class Settings extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: 'SETTINGS',
+    headerRight: (
+        <RightHeaderButtons navigation={navigation}/>
+    ),
+  });
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Settings</Text>
-      </View>
+      <Container>
+        <Content>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#FF9501" }}>
+                <Icon active name="plane" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Airplane Mode</Text>
+            </Body>
+            <Right>
+              <Switch value={false} />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="wifi" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Wi-Fi</Text>
+            </Body>
+            <Right>
+              <Text>GeekyAnts</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="bluetooth" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Bluetooth</Text>
+            </Body>
+            <Right>
+              <Text>On</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+        </Content>
+      </Container>
     );
   }
 }
 export default Settings;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
